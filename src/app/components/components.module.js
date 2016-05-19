e2abbd72962cb29d15f31a416c367dda3570384e
ghoullier <gregory.houllier@gmail.com>
2016-05-19 10:35:45 +0200

    feat(login) Remove Login Component

--- src/app/components/components.module.js
+++ src/app/components/components.module.js
@@ -1,7 +1,5 @@
-import { login } from './login/login.module'
 import { profile } from './profile/profile.module'
 
 export const components = angular.module('io.cfp.front.components', [
-  login.name,
   profile.name
 ])
